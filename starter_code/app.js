3a3a872e6b3b2d24e558fad8cc226053543c7206
iMAC <iMAC@MacBook-Pro-de-MACBOOK.local>
2019-04-03 22:16:51 +0200

    Iteration 3

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,26 +1,74 @@
+//--00. REQUIRES + EXPRESS APP
+
 const express = require('express');
 const hbs = require('hbs');
-
+const prettyjson = require('prettyjson');
 // require spotify-web-api-node package here:
-
-
+var SpotifyWebApi = require('spotify-web-api-node');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+//--0. SETUP
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+hbs.registerPartials(__dirname + '/views/partials');
+/* setting the spotify-api goes here:
+Client ID d3b762eba6f14bdf9c3a6365ff757c42
+Client Secret 67768cda89af44a2942861aed4a0b089*/
+// Remember to insert your credentials here
+const clientId = 'd3b762eba6f14bdf9c3a6365ff757c42',
+    clientSecret = '67768cda89af44a2942861aed4a0b089';
 
-// setting the spotify-api goes here:
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
 
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then( data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
 
+//--1. ROUTES
 
+app.get('/', (req, res, next) => {
+    res.render('index');
+  });
 
+app.post('/artists', (req, res, next) => {
 
+    let artist=req.body.artist;
+    console.log(req.body);
+    let artistsInfo;
+    spotifyApi.searchArtists(artist)
+    .then(data => {
 
-// the routes go here:
+      console.log("The received data from the API: ", prettyjson.render(data.body));
+      let d=JSON.stringify(data.body);
 
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      res.send(`${d}`);
 
+    })
+    .catch(err => {
+      console.log("The error while searching artists occurred: ", err);
+    })
+  });  
+  
+
+  app.get('/albums', (req, res, next) => {
+    res.render('albums');
+  });
+
+//--2. SERVER LISTENING
 
 app.listen(3000, () => console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊"));
